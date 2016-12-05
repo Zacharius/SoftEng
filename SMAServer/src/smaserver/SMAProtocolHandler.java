b701bccfbfef55c8035feca0609557ecbd90f307
Elijah Arnold <elijah.m.arnold@gmail.com>
2016-12-05 13:00:22 -0600

    added contact request routing to SMAProtocolHandler

--- SMAServer/src/smaserver/SMAProtocolHandler.java
+++ SMAServer/src/smaserver/SMAProtocolHandler.java
@@ -2,6 +2,10 @@ package smaserver;
 
 import com.google.gson.Gson;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -78,6 +82,9 @@ public class SMAProtocolHandler {
                     reason = "failed to update user password.";
                 }
                 break;
+            case 4:
+                printClientLogMessage(clientID, "handling contact request");
+                return sendContactRequest(input, clientID);
             case 8:
                 SMAChangePublicKeyRequest newPublicKey = gson.fromJson(input, SMAChangePublicKeyRequest.class);
                 if(DBAccess.changePubKey(clientID, newPublicKey.getPublicKey())){
@@ -98,5 +105,63 @@ public class SMAProtocolHandler {
                 status,
                 reason);
         return gson.toJson(response);
+    }
+
+    /**
+     * Given the input line and clientID, return a server response message to a contact request.
+     * @param input represents a line of input. Type should be checked prior to calling this.
+     * @param clientID is the clientID sending the request.
+     * @return is a String to be sent to the client in response to this request.
+     */
+    private String sendContactRequest(String input, String clientID){
+        // Create an SMAContactRequestMessage object.
+        SMAContactRequestMessage contactRequest = gson.fromJson(input, SMAContactRequestMessage.class);
+
+        printClientLogMessage(clientID, "contact request recipient is " + contactRequest.getRecipient());
+
+        // See if the contact request can be sent by checking if the user exists or not.
+        if(!DBAccess.userExists(contactRequest.getRecipient())){
+
+            printClientLogMessage(clientID, "contact request failed, " + contactRequest.getRecipient() + " does not exist");
+            return gson.toJson(new SMANetworkResponse(
+                    9,
+                    contactRequest.getMessageID(),
+                    false,
+                    "INVALID USER: user does not exist"
+            ));
+        }else{
+            printClientLogMessage(clientID, contactRequest.getRecipient() + " exists, routing request");
+            // Insert this message into the database to be sent later.
+            if(!DBAccess.addMessage(clientID, contactRequest.getRecipient(), null, new Timestamp(Calendar.getInstance().get(Calendar.MILLISECOND)), 5, contactRequest.getMessageID())){
+                printClientLogMessage(clientID, "request could not be sent, database access failed");
+                return gson.toJson(new SMANetworkResponse(
+                        9,
+                        contactRequest.getMessageID(),
+                        false,
+                        "DB ACCESS FAILURE: could not route contact request"
+                ));
+            }else{
+                printClientLogMessage(clientID, "request sent");
+                return gson.toJson(new SMANetworkResponse(
+                        9,
+                        contactRequest.getMessageID(),
+                        true,
+                        null
+                ));
+            }
+        }
+    }
+
+    /**
+     * Print a log messages with the [CLIENT] tag to console
+     * TODO: possibly create a separate logging Thread to ensure console is clean while
+     * administrators are interacting with it
+     * @param clientID is the client we're associating this logging message with.
+     * @param msg is the message we want to print to console.
+     */
+    private void printClientLogMessage(String clientID, String msg){
+        DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println("[" + sdf.format(date) + "][CLIENT: " + clientID + "]: " + msg);
     }
 }
